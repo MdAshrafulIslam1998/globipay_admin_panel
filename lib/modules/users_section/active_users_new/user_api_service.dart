c8589c5ac1ce61e033419fd677ebf8d9fa6231f8
Md. Ashraful Islam <mdashrafulislam.inbox@gmail.com>
2024-11-15 02:41:31 +0600

    api not fixed

--- lib/modules/users_section/active_users_new/user_api_service.dart
+++ lib/modules/users_section/active_users_new/user_api_service.dart
@@ -2,20 +2,17 @@ import 'package:dio/dio.dart';
 import 'user_new_response_model.dart';
 
 class UserApiService {
-  final Dio _dio = Dio();
-
-  // Consider using a secure environment variable for the JWT token
+  final Dio dio = Dio();
+  
   final String jwtToken =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI3NzQ2YzE5LTdhNjEtMTFlZi04MjExLTgwZmE1Yjg4OGM5YSIsImlhdCI6MTczMTU5MjIyNywiZXhwIjoxNzMxNzAwMjI3fQ.OZRyCCW7rTVfVFCAulMfSf0lokUHfa686poFuLnQM0o"; // Replace with your actual token
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImI3NzQ2YzE5LTdhNjEtMTFlZi04MjExLTgwZmE1Yjg4OGM5YSIsImlhdCI6MTczMTU5MjIyNywiZXhwIjoxNzMxNzAwMjI3fQ.OZRyCCW7rTVfVFCAulMfSf0lokUHfa686poFuLnQM0o";
 
   Future<UserResponse?> fetchUsers(int page, int limit) async {
-    const String baseUrl = 'https://www.projectzerotwofour.cloudns.ch/api/user';
-    final String endpoint =
-        '/verifiedusersweb'; // Assuming this is the correct endpoint
-
+    final String url = 'https://www.projectzerotwofour.cloudns.ch/api/user/verifiedusersweb';
+    
     try {
-      final response = await _dio.get(
-        baseUrl + endpoint,
+      final response = await dio.get(
+        url,
         queryParameters: {
           'page': page,
           'limit': limit,
@@ -35,9 +32,6 @@ class UserApiService {
       }
     } catch (e) {
       print("Error occurred while fetching users: ${e.toString()}");
-      if (e is DioError) {
-        print("DioError type: ${e.type}, message: ${e.message}");
-      }
       return null;
     }
   }
