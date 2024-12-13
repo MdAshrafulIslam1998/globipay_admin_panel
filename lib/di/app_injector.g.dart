98baf4be859b9a22ef3f8459a9e9f9d17e249ced
Abdullah <mmunna864@gmail.com>
2024-12-14 01:50:38 +0600

    conflict issue resolved

--- lib/di/app_injector.g.dart
+++ lib/di/app_injector.g.dart
@@ -30,13 +30,6 @@ class _$AppInjector extends AppInjector {
       ..registerFactory((c) => CreateCategoryController())
       ..registerFactory((c) => TransactionHistoryController())
       ..registerFactory((c) => UserAmountController())
-      ..registerFactory((c) => UserLevelsController())
-      ..registerFactory((c) => BlockedUsersController(
-          c.resolve<AppRepository>(), c.resolve<TokenRepository>()))
-      ..registerFactory((c) => PendingUsersController(
-          c.resolve<AppRepository>(), c.resolve<TokenRepository>()))
-      ..registerFactory((c) => ActiveUsersNewController(
-          c.resolve<AppRepository>(), c.resolve<TokenRepository>()))
       ..registerFactory((c) => UserLevelsController(
           c.resolve<AppRepository>(), c.resolve<TokenRepository>()))
       ..registerFactory((c) => BlockedUsersController(
